edd11ed194ea5340e6eb7f96c7c8e54dafd50f0a
P47Parzival <dhruvmali999@gmail.com>
2025-04-05 09:46:39 +0530

    Yes boss activated

--- main.py
+++ main.py
@@ -9,5 +9,29 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processcommand():
+    pass
+
 if __name__ == "__main__":
-    speak("Initializing Jarvis") 
+    speak("Initializing Kaizen...")
+    while True:
+        r = sr.Recognizer()
+            # recognize speech using google
+        print("Recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=5, phrase_time_limit=10)
+            word = r.recognize_google(audio)
+            print(word)
+            if word.lower() =="kaizen":
+                speak("Yes Boss!")
+                with sr.Microphone() as source:
+                    print("Kaizen activated...")
+                    audio = r.listen(source, timeout=5, phrase_time_limit=10)
+                    command = r.recognize_google(audio)
+
+                    processcommand()
+                           
+        except Exception as e:
+            print("Error occured; {0}".format(e))
