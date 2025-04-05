16c2384b0fe039fd384be427fa9eb93d59e5188e
P47Parzival <dhruvmali999@gmail.com>
2025-04-05 09:59:03 +0530

    opens google

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-def processcommand():
-    pass
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://www.google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://www.youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://www.linkedin.com")
+
 
 if __name__ == "__main__":
     speak("Initializing Kaizen...")
@@ -21,17 +27,17 @@ if __name__ == "__main__":
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source, timeout=5, phrase_time_limit=10)
+                audio = r.listen(source, timeout=5, phrase_time_limit=5)
             word = r.recognize_google(audio)
             print(word)
             if word.lower() =="kaizen":
                 speak("Yes Boss!")
                 with sr.Microphone() as source:
                     print("Kaizen activated...")
-                    audio = r.listen(source, timeout=5, phrase_time_limit=10)
+                    audio = r.listen(source, timeout=5, phrase_time_limit=5)
                     command = r.recognize_google(audio)
 
-                    processcommand()
+                    processcommand(command)
                            
         except Exception as e:
             print("Error occured; {0}".format(e))
